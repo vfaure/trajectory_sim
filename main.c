f4a76d62d6f570159822df79e4cab3b6072e13a9
vfaure <vincent.hr.faure@gmail.com>
2015-11-29 01:25:52 +0100

    Adaptation du makefile pour creer la table et la dessiner en sdl

--- main.c
+++ main.c
@@ -2,13 +2,28 @@
 #include <stdlib.h>
 
 #include "map.h"
-
+#include "table.h"
 
 int main(int argc, const char* argv[])
 {
-    printf("HelloWorld!\n");
+    //Create the table
+    uint32_t ** p_table = NULL;
+    p_table = createTable(300, 200);
+    
+    //Fill the table
+    setRectangleOccupied(p_table, 2, 20, 80, 0);
+    setRectangleOccupied(p_table, 2, 20, 220, 0);
+    
+    setRectangleOccupied(p_table, 120, 2, 90, 75);
+    setRectangleOccupied(p_table, 6, 60, 147, 75);
+    
+    setRectangleOccupied(p_table, 1, 10, 0, 150);
+    
+    setCircleOccupied(p_table, 0, 199, 25); 
+    setCircleOccupied(p_table, 299, 199, 25); 
+
     init_map();
-    draw_map();
+    draw_map(p_table, 300, 200, 2);
     sdl_pause();
     quit_map();
     return EXIT_SUCCESS;
